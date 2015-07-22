9c6cb90e2ef88bc7c5b4050d7b158fcac45900d1
Chris Dufour <chrisdufour@hotmail.ca>
2015-07-21 23:01:55 -0400

    Fixed server errors on invalid data and added proper json replies to errors

---
+++ FaceTag/src/faceTag/entities/ErrorCode.java
@@ -0,0 +1,13 @@
+package faceTag.entities;
+
+public class ErrorCode {
+	public static final int ERROR_BAD_PARAMETERS = 1;
+	public static final int ERROR_RESOURCE_NOT_FOUND = 2;
+	public static final int ERROR_CANT_BE_FRIENDS = 10;
+	public static final int ERROR_ALREADY_FIRENDS = 11;
+	public static final int ERROR_NOT_FRIENDS = 12;
+	public static final int ERROR_INVALID_CREDENTIALS = 20;
+	public static final int ERROR_USERNAME_IN_USE = 21;
+	public static final int ERROR_BAD_TOKEN = 30;
+	public static final int ERROR_BAD_IMAGE_FORMAT = 40;
+}
